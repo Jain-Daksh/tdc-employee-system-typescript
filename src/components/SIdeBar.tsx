b1f0c7f3485b8fc3317b2f9c82c3c9076df6ee17
daksh <daksh44jain@gmail.com>
2023-07-27 11:21:38 +0530

    Added sidebar

--- src/components/SIdeBar.tsx
+++ src/components/SIdeBar.tsx
@@ -1,21 +1,56 @@
 import { Layout, Menu } from 'antd'
+import Link from 'next/link'
 
 const { Sider, Content } = Layout
 
 interface Props {
   children: any
+  links: { label: string; href: string; color: string }[]
 }
 
-const Sidebar: React.FC<Props> = ({ children }) => {
+const sidebar = [
+  {
+    id: 1,
+    key: 1,
+    link: '/',
+    Name: 'Home'
+  },
+  {
+    id: 2,
+    key: 2,
+    link: '/add-user',
+    Name: 'Add User'
+  },
+  {
+    id: 3,
+    key: 3,
+    link: '/table',
+    Name: 'User Table'
+  }
+]
+const Sidebar: React.FC<Props> = ({ children, links }) => {
   return (
     <Layout style={{ minHeight: '100vh' }}>
       <Sider width={200} theme="light">
         <div className="logo" />
         <Menu theme="light" mode="inline" defaultSelectedKeys={['1']}>
-          {/* Add your sidebar items here */}
-          <Menu.Item key="1">Dashboard</Menu.Item>
+          {/* <Link href="/">
+            <Menu.Item key="1">Dashboard</Menu.Item>
+          </Link>
+          <Link href="/">
+            <Menu.Item key="1">User</Menu.Item>
+          </Link>
+          <Link href="/">
+            <Menu.Item key="1">Add</Menu.Item>
+          </Link> */}
+          {/* <Menu.Item key="1"></Menu.Item>
           <Menu.Item key="2">Users</Menu.Item>
-          <Menu.Item key="3">Settings</Menu.Item>
+          <Menu.Item key="3">Settings</Menu.Item> */}
+          {sidebar.map(value => (
+            <Link key={value.id} href={value.link}>
+              <Menu.Item key={value.key}>{value.Name}</Menu.Item>
+            </Link>
+          ))}
         </Menu>
       </Sider>
       <Layout>
