f7cfc103f41e033f89430324bd524634fc6552eb
daksh <daksh44jain@gmail.com>
2023-07-27 13:25:26 +0530

    fixed error

--- src/components/UserTable.tsx
+++ src/components/UserTable.tsx
@@ -1,206 +1,14 @@
-// import React from 'react'
-
-// const UserTable = () => {
-//   return (
-//     <div>
-
-//     </div>
-//   )
-// }
-
-// export default UserTable
-
-// import { UserOutlined } from '@ant-design/icons'
-// import { Button, Form, Input, Modal, Table } from 'antd'
-// import { useEffect, useState } from 'react'
-// // import Header from './common/Header'
-
-// // interface User : {
-// //   user: any
-// // }
-
-// const UserTable = () => {
-//   // const [users, setUsers] = useState([])
-//   const [users, setUsers] = useState<User[]>([])
-//   const [visible, setVisible] = useState(false)
-//   const [selectedUser, setSelectedUser] = useState(users)
-//   const [EditUSer, setEditUser] = useState(users)
-
-//   // const [viewModalVisible, setViewModalVisible] = useState(false);
-//   // const [editModalVisible, setEditModalVisible] = useState(false);
-//   // const [selectedUser, setSelectedUser] = useState<User | null>(null);
-//   // const [editedUser, setEditedUser] = useState<User | null>(null);
-
-//   const columns = [
-//     {
-//       title: 'Name',
-//       dataIndex: 'name',
-//       key: 'name'
-//     },
-//     {
-//       title: 'Email',
-//       dataIndex: 'email',
-//       key: 'email'
-//     },
-//     {
-//       title: 'Organization',
-//       dataIndex: 'organization',
-//       key: 'organization'
-//     },
-//     {
-//       title: 'Actions',
-//       dataIndex: 'actions',
-//       key: 'actions',
-//       render: (_, user) => (
-//         <>
-//           <Button icon={<UserOutlined />} onClick={() => handleViewUser(user)}>
-//             View
-//           </Button>
-//           <Button
-//             icon={<UserOutlined />}
-//             onClick={() => handleEditUser(user)}
-//             style={{ marginLeft: 8 }}
-//           >
-//             Edit
-//           </Button>
-//         </>
-//       )
-//     }
-//   ]
-//   useEffect(() => {
-//     async function fetchUsers() {
-//       try {
-//         const response = await fetch('/api/getUsers')
-//         const data = await response.json()
-//         console.log('data', data)
-//         setUsers(data)
-//       } catch (error) {
-//         console.error(error)
-//       }
-//     }
-//     fetchUsers()
-//   }, [])
-//   console.log(users)
-//   // console.log(users.data.email)
-//   const handleViewUser = user => {
-//     setSelectedUser(user)
-//     setVisible(true)
-//   }
-
-//   const handleEditUser = user => {
-//     setSelectedUser(user)
-//     setVisible(true)
-//   }
-
-//   const handleCancel = () => {
-//     setSelectedUser(null)
-//     setVisible(false)
-//   }
-//   // const handleSaveEdit = () => {
-//   //   // Save the edited user to the backend using API call
-//   //   // For this example, we'll just update the user in the local state
-//   //   if (EditUSer) {
-//   //     setUsers(prevUsers => {
-//   //       const updatedUsers = prevUsers.map(user =>
-//   //         user.id === EditUSer.id ? { ...user, ...EditUSer } : user
-//   //       )
-//   //       return updatedUsers
-//   //     })
-//   //     setVisible(false)
-//   //     setEditUser(null)
-//   //   }
-//   // }
-
-//   // const handleViewUser = (user: User) => {
-//   //   setSelectedUser(user)
-//   //   setViewModalVisible(true)
-//   // }
-
-//   // const handleEditUser = (user: User) => {
-//   //   setSelectedUser(user)
-//   //   setEditedUser({ ...user })
-//   //   setEditModalVisible(true)
-//   // }
-
-//   // const handleCancelView = () => {
-//   //   setViewModalVisible(false)
-//   // }
-
-//   // const handleCancelEdit = () => {
-//   //   setEditModalVisible(false)
-//   //   setEditedUser(null)
-//   // }
-
-//   // const handleSaveEdit = () => {
-//   //   // Save the edited user to the backend using API call
-//   //   // For this example, we'll just update the user in the local state
-//   //   if (editedUser) {
-//   //     setUsers(prevUsers => {
-//   //       const updatedUsers = prevUsers.map(user =>
-//   //         user.id === editedUser.id ? { ...user, ...editedUser } : user
-//   //       )
-//   //       return updatedUsers
-//   //     })
-//   //     setEditModalVisible(false)
-//   //     setEditedUser(null)
-//   //   }
-//   // }
-
-//   return (
-//     <>
-//       {/* <Header title="Employee" subtitle="Entire list " /> */}
-//       <Table dataSource={users} columns={columns} />
-
-//       <Modal
-//         title="User Details"
-//         open={visible}
-//         onCancel={handleCancel}
-//         footer={null}
-//       >
-//         {selectedUser && (
-//           <Form layout="vertical">
-//             <Form.Item label="Name">
-//               <Input value={selectedUser.name} readOnly />
-//             </Form.Item>
-//             <Form.Item label="Email">
-//               <Input value={selectedUser.email} readOnly />
-//             </Form.Item>
-//           </Form>
-//         )}
-//       </Modal>
-//       <Modal
-//         title="User Details"
-//         open={visible}
-//         onCancel={handleCancel}
-//         footer={null}
-//       >
-//         {EditUSer && (
-//           <Form layout="vertical">
-//             <Form.Item label="Name">
-//               <Input value={selectedUser.name} readOnly />
-//             </Form.Item>
-//             <Form.Item label="Email">
-//               <Input value={selectedUser.email} readOnly />
-//             </Form.Item>
-//           </Form>
-//         )}
-//       </Modal>
-//     </>
-//   )
-// }
-
-// export default UserTable
-
 import { UserOutlined } from '@ant-design/icons'
 import { Button, Form, Modal, Pagination, Table } from 'antd'
 import Input from 'rc-input'
 import { useEffect, useState } from 'react'
 
 const PAGE_SIZE = 10
-
 interface Users {
+  id: string
   name: string
   email: string
+  organization: string
 }
 
 const EmployeesPage: React.FC = () => {
@@ -210,6 +18,10 @@ const EmployeesPage: React.FC = () => {
 
   const [visible, setVisible] = useState(false)
   const [selectedUser, setSelectedUser] = useState<Users | null>()
+  const [userEdit, setUserEdit] = useState<Users | null>()
+
+  const [editModalVisible, setEditModalVisible] = useState(false)
+  const [editedUser, setEditedUser] = useState<Users | null>(null)
 
   const handleCancel = () => {
     setSelectedUser(null)
@@ -233,7 +45,32 @@ const EmployeesPage: React.FC = () => {
     }
     fetchUsers()
   }, [])
-  console.log(employees)
+
+  const handleEditUser = (employee: Users) => {
+    setSelectedUser(employee)
+    setEditedUser({ ...employee })
+    setEditModalVisible(true)
+  }
+
+  const handleCancelEdit = () => {
+    setEditModalVisible(false)
+    setEditedUser(null)
+  }
+
+  const handleSaveEdit = () => {
+    // Save the edited user to the backend using API call
+    // For this example, we'll just update the user in the local state
+    if (editedUser) {
+      setEmployees(prevUsers => {
+        const updatedUsers = prevUsers.map(user =>
+          user.id === editedUser.id ? { ...user, ...editedUser } : user
+        )
+        return updatedUsers
+      })
+      setEditModalVisible(false)
+      setEditedUser(null)
+    }
+  }
 
   const handlePageChange = (page: any) => {
     setCurrentPage(page)
@@ -277,13 +114,13 @@ const EmployeesPage: React.FC = () => {
           >
             View
           </Button>
-          {/* <Button
+          <Button
             icon={<UserOutlined />}
-            onClick={() => handleEditUser(user)}
+            onClick={() => handleEditUser(employees)}
             style={{ marginLeft: 8 }}
           >
             Edit
-          </Button> */}
+          </Button>
         </>
       )
     }
@@ -314,6 +151,28 @@ const EmployeesPage: React.FC = () => {
             </Form.Item>
             <Form.Item label="Email">
               <Input value={selectedUser.email} readOnly />
+            </Form.Item>
+            <Form.Item label="Organization">
+              <Input value={selectedUser.organization} readOnly />
+            </Form.Item>
+          </Form>
+        )}
+      </Modal>
+      <Modal
+        title="Edit User"
+        visible={editModalVisible}
+        onCancel={handleCancelEdit}
+        onOk={handleSaveEdit}
+      >
+        {editedUser && (
+          <Form layout="vertical">
+            <Form.Item label="Name">
+              <Input
+                value={editedUser.name}
+                onChange={e =>
+                  setEditedUser({ ...editedUser, name: e.target.value })
+                }
+              />
             </Form.Item>
           </Form>
         )}
