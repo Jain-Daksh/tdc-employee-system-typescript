d00beaaab5730780e12776b74ac93eaf72b6451f
daksh <daksh44jain@gmail.com>
2023-07-28 11:32:46 +0530

    removed commented code

--- src/pages/api/addUser.ts
+++ src/pages/api/addUser.ts
@@ -1,40 +1,3 @@
-// import { PrismaClient } from '@prisma/client'
-// import { NextApiRequest, NextApiResponse } from 'next'
-// const prisma = new PrismaClient()
-
-// import bcrypt from 'bcrypt'
-
-// export default async function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse
-// ) {
-//   if (req.method !== 'POST') {
-//     return res.status(405).json({ message: 'Method not allowed' })
-//   }
-//   const { name, password, email, organization } = req.body
-//   const existingUser = await prisma.user.findUnique({ where: { email } })
-
-//   if (existingUser) {
-//     return res.status(409).json({ message: 'Email already exists' })
-//   }
-//   try {
-//     const hashedPassword = await bcrypt.hash(password, 10)
-
-//     const user = await prisma.user.create({
-//       data: {
-//         name,
-//         email,
-//         password: hashedPassword,
-//         organization
-//       }
-//     })
-
-//     return res.status(201).json({ message: 'User added successfully', user })
-//   } catch (error) {
-//     return res.status(500).json({ message: 'Error adding user', error })
-//   }
-// }
-
 import { PrismaClient } from '@prisma/client'
 import bcrypt from 'bcrypt'
 import { NextApiRequest, NextApiResponse } from 'next'
@@ -49,7 +12,6 @@ export default async function handler(
   }
 
   try {
-    // const { name, password, email, organization, roleId } = req.body
     const { name, password, email, organization, roleId } = req.body
 
     const hashedPassword = await bcrypt.hash(password, 10)
