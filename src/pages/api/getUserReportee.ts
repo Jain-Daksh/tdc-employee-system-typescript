b511a42b583944ac28947e0e12bfe307d76e581a
daksh <daksh44jain@gmail.com>
2023-07-28 14:01:48 +0530

    Added getuserrole

---
+++ src/pages/api/getUserReportee.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from '@prisma/client'
+import { NextApiRequest, NextApiResponse } from 'next'
+const prisma = new PrismaClient()
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== 'GET') {
+    return res.status(405).json({ message: 'Method not allowed' })
+  }
+
+  try {
+    const users = await prisma.user_reportee.findMany({
+      include: {
+        manager: true,
+        reportee: true
+      }
+    })
+    return res.status(200).json(users)
+  } catch (error) {
+    console.error(error)
+    return res.status(500).json({ message: 'Something went wrong' })
+  }
+}
