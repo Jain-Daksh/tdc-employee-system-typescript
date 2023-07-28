878de10a53219bc89faad6dab339118e7a843e25
daksh <daksh44jain@gmail.com>
2023-07-28 16:36:19 +0530

    test

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,55 @@
-// import '@/styles/common.css'
-// import '@/styles/globals.css'
-// import type { AppProps } from 'next/app'
+// // import '@/styles/common.css'
+// // import '@/styles/globals.css'
+// // import type { AppProps } from 'next/app'
 
-// export default function App({ Component, pageProps }: AppProps) {
-//   return <Component {...pageProps} />
+// // export default function App({ Component, pageProps }: AppProps) {
+// //   return <Component {...pageProps} />
+// // }
+
+// import 'antd/dist/reset.css' // Import Ant Design CSS
+// import SIdeBar from '../components/SIdeBar'
+// import '../styles/common.css'
+// // import Login from './login'
+
+// interface Props {
+//   Component: any
+//   pageProps: any
 // }
+
+// const MyApp: React.FC<Props> = ({ Component, pageProps }) => {
+//   const isLoggedIn = true
+//   // if (!isLoggedIn) {
+//   //   return <Login />
+//   // }
+//   return (
+//     <SIdeBar>
+//       <Component {...pageProps} />
+//     </SIdeBar>
+//   )
+// }
+
+// export default MyApp
 
 import 'antd/dist/reset.css' // Import Ant Design CSS
 import SIdeBar from '../components/SIdeBar'
 import '../styles/common.css'
 // import Login from './login'
-
+import { useEffect, useState } from 'react'
 interface Props {
   Component: any
   pageProps: any
 }
 
 const MyApp: React.FC<Props> = ({ Component, pageProps }) => {
-  const isLoggedIn = true
-  // if (!isLoggedIn) {
-  //   return <Login />
-  // }
+  const [authToken, setAuthToken] = useState('')
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const token = window.localStorage.getItem('authToken')
+      setAuthToken(token)
+    }
+  }, [])
+
   return (
     <SIdeBar>
       <Component {...pageProps} />
